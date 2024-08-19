949bb5c0ce121dd5b4aeb399fabc3bdf127740a9
yoann-dufresne <yoann.dufresne0@gmail.com>
2024-08-19 16:51:05 +0200

    :heavy_plus_sign: Google test added

--- src/include/Multiply.hpp
+++ src/include/Multiply.hpp
@@ -18,16 +18,63 @@ private:
     size_t m_val_size;
 
 public:
-    Multiply(myuint multiplier, size_t val_size) : m_multiplier(multiplier), m_val_size(val_size) {}
+    Multiply(myuint multiplier, size_t val_size) : m_multiplier(multiplier), m_val_size(val_size) {
+        // Compute the pgcd between the multiplier and pow(2, val_size) to make sure that the multiplier is invertible
+        myuint a = m_multiplier;
+        myuint b = 1 << (m_val_size - 1);
+        myuint r;
+        while (b != 0)
+        {
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        if (a != 1)
+        {
+            throw std::runtime_error("The multiplier is not invertible");
+        }
+    }
+
     Multiply(Multiply const & other) : Multiply(other.m_multiplier, other.m_val_size) {}
     ~Multiply() {}
+
+    myuint get_invert_multiplier() const
+    {
+        // Compute the inverse of the multiplier by using the extended euclidean algorithm (cf https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm)
+        myuint a = m_multiplier;
+        myuint b = 1 << (m_val_size - 1);
+        myuint r;
+        myuint u = 1;
+        myuint v = 0;
+        myuint u1 = 0;
+        myuint v1 = 1;
+        myuint q;
+        myuint t;
+        while (b != 0)
+        {
+            q = a / b;
+            r = a % b;
+            a = b;
+            b = r;
+            t = u;
+            u = u1;
+            u1 = t - q * u1;
+            t = v;
+            v = v1;
+            v1 = t - q * v1;
+        }
+        return u % (1 << (m_val_size - 1));
+    }
 
     // Implement the invert function
     std::vector<std::unique_ptr<Operator<myuint>>> invert() const override
     {
         std::vector<std::unique_ptr<Operator<myuint>>> inverted;
         
-        throw std::runtime_error("Not implemented yet");
+        // Compute the inverse of the multiplier
+        myuint inv_multiplier = get_invert_multiplier();
+        // Register the inverse operator
+        inverted.push_back(std::make_unique<Multiply<myuint>>(inv_multiplier, m_val_size));
 
         return inverted;
     }
